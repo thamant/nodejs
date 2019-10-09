e0eaae611b11e3d9535799a14148995e6da36029
atham4 <atham4@myseneca.ca>
2019-10-08 20:04:37 -0400

    endpoints, default values

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
 const app = express();
 
 // Setup handlebars engine and views location
@@ -35,9 +38,46 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'Address must be provided'
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({
+          error
+        });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({
+            error
+          });
+        }
+        res.send({
+          location,
+          forecast: forecastData,
+          address: req.query.address
+        });
+      });
+    }
+  );
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    });
+  }
+  console.log(req.query.search);
   res.send({
-    forecast: 'Sunny',
-    location: 'Toronto'
+    products: []
   });
 });
 

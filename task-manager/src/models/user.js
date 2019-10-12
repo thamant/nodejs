4fe6aa86ce03a47bb70899d8522066145b730ca0
atham4 <atham4@myseneca.ca>
2019-10-12 16:33:18 -0400

    Middleware, Login

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   age: {
     type: Number,
@@ -14,6 +15,7 @@ const User = mongoose.model('User', {
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -35,5 +37,33 @@ const User = mongoose.model('User', {
     }
   }
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+// Hash the plain text password
+userSchema.pre('save', async function(next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -34,6 +34,19 @@ router.post('/users', async (req, res) => {
   }
 });
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
 router.patch('/users/:id', async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['name', 'email', 'password', 'age'];
@@ -46,10 +59,14 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+    const user = await User.findById(req.params.id);
+    updates.forEach(update => (user[update] = req.body[update]));
+    await user.save();
+
+    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true
+    // });
 
     if (!user) {
       return res.status(404).send();
